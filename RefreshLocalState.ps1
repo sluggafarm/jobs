f653e687dd802c765981a721895955c20c71d6f3
Slugga Farm <125496910+sluggafarm@users.noreply.github.com>
2023-02-23 10:47:10 -0500

    Update RefreshLocalState.ps1
    added some refinement to the output

--- RefreshLocalState.ps1
+++ RefreshLocalState.ps1
@@ -1,4 +1,4 @@
-# Slugga Farmer v1.0
+# Slugga State Downloader v1.3
 $wallet = "[Your Wallet Here]"
 $local_cache = "C:\temp\_slugga\"
 $baseurl = "https://pastelworld.io/slugga-api/api/v1"
@@ -6,15 +6,29 @@ $my_ids = "4", "5", "6"
 
 Function Run-SluggaRefreshState {
     param([string]$id)
-    $url = "$baseurl/slug/$id/$wallet"
-    Write-Host $url
-    $outPath = "$($local_cache)$($id).json"
-    $rr = Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $outPath -ErrorAction Continue
+
+    try {
+        $url = "$baseurl/slug/$id/$wallet"
+        Write-Host $id -NoNewLine
+        $outPath = "$($local_cache)$($id).json"
+        $rr = Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $outPath -ErrorAction Continue
+    } catch { 
+        Write-Host "!!!" -f Red -NoNewline
+    }
+    WRite-Host " " -NoNewLine
 }
-Function Run-SluggaRefreshState-ForAll {   
+
+Function Run-SluggaRefreshState-ForAll {
+    $newline_countdown = 10
     foreach($id in $my_ids) {
         Run-SluggaRefreshState -id $id
-        Sleep -Seconds 3
+        Sleep -Milliseconds 2500
+        $newline_countdown--
+        if ($newline_countdown -eq 0) {
+            $newline_countdown = 10
+            Write-Host
+        }
     }
 }
+
 Run-SluggaRefreshState-ForAll
